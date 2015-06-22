a13cfc319fd5faed238fc664620a3ca90ae1c738
edalatian <jk>
2015-06-22 16:52:11 +0430

    shit

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,21 @@
 var gpio = require('rpi-gpio');
 var express = require('express');
 var router = express.Router();
-
+var port = 0;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/pin/:id/:state', function(req, res, next) {
-  
-  gpio.setup(27, gpio.DIR_OUT, write);
+  port = req.params.id;
+  gpio.setup(req.params.id, gpio.DIR_OUT, write);
     
   res.send('pin' + req.params.id + ' is ' + req.params.state);
 });
 
  function write() {
-      gpio.write(27, true, function(err) {
+      gpio.write(port, true, function(err) {
           if (err) throw err;
           console.log('Written to pin');
       });
