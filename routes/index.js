aadd1d4a21ce0fcf0fb72188ed016c526d65ab90
edalatian <jk>
2015-06-22 17:02:29 +0430

    Repair rout

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,18 @@ var gpio = require('rpi-gpio');
 var express = require('express');
 var router = express.Router();
 var port = 0;
+
+gpio.on('export', function(channel) {
+    console.log('Channel set: ' + channel);
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/pin/:id/:state', function(req, res, next) {
-  port = req.params.id;
-  gpio.setup(req.params.id, gpio.DIR_OUT, write);
+  port = Number(req.params.id);
+  gpio.setup(port, gpio.DIR_OUT, write);
     
   res.send('pin' + req.params.id + ' is ' + req.params.state);
 });
